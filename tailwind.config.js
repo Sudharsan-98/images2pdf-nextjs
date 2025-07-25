bae9ae12250cee56c3a31c3dc644cbb28c56389e
Sudharsan <135497380+Sudharsan-98@users.noreply.github.com>
2025-07-26 00:15:29 +0530

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
+// tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // If using App Router
+    "./src/**/*.{js,ts,jsx,tsx}",  // If using src/ folder
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 }
