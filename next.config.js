0a416b69dabc17fac56b9f02fd1a201ee010cd05
Sudharsan <135497380+Sudharsan-98@users.noreply.github.com>
2025-07-25 23:37:27 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+};
